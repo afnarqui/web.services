9d54a268de25ffac78dde7448847f4942a4ee9dd
afnarqui <afnarqui@hotmail.com>
2015-05-15 10:17:13 -0500

    subir versión con el modelo
    estoy mapeando la entidad copia

---
+++ WebApplication1/WebApplication1/Datos/CopiaDatos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Negocio;
+
+
+namespace WebApplication1.Datos
+{
+    internal class CopiaDatos
+    {
+        /// <summary>
+        /// ANDRES NARANJO 2015-05-15 devuelve una lista de la entidad copia
+        /// </summary>
+        /// <returns></returns>
+        public List<copia> BuscarDatosCopia()
+        {
+
+
+            using (DatosopawebEntities bd = new DatosopawebEntities())
+            {
+
+                return bd.copias.ToList();
+
+            }
+
+
+        }
+
+
+
+    }
+}
